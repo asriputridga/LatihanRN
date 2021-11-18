b924499c7d38ac55bbcc50f9e857e10e69821984
Asri Putri Dwi Gita Andini <asriandin1505@gmail.com>
2021-11-18 14:01:53 +0700

    bismillah bisa

---
+++ style.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        margin: 20,
+    },
+    containerig: {
+        flex: 1,
+        // padding: 10,
+        margin: 10,
+    },
+    tulisanku: {
+        marginTop: 15,
+        paddingVertical: 8,
+        borderWidth: 3,
+        fontSize: 20
+    },
+    gambarpanel: {
+        width: 25,
+        height: 25,
+        borderRadius: 25,
+    },
+    instagram: {
+        fontWeight: "bold",
+        fontSize: 24,
+        fontFamily: "Cochin"
+    }
+});
+
+export { style };
